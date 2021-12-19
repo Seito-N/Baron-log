dd01fc63477e2a0893dd8ff3697c9aedd2ed1eab
Seito Nakamura <74122643+Seito-N@users.noreply.github.com>
2021-12-19 21:21:05 +0900

    update app. add bar plot.

--- Baron-log-app/server.R
+++ Baron-log-app/server.R
@@ -1,27 +1,49 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(tidyverse)
+library(lubridate)
 
 # Define server logic required to draw a histogram
+
 shinyServer(function(input, output) {
     
-    bins <- reactive({
-        x = faithful[, 2]
-        return(seq(min(x), max(x), length.out = input$bins + 1))
+    ## in data
+    dat <- readxl::read_xlsx("C:/Users/seito/Documents/Baron-log/baron_log.xlsx")
+    
+    ## make data
+    dat2 <- dat %>% 
+        mutate(date = ymd(date)) %>% 
+        mutate(time = hms::as_hms(time)) %>% 
+        mutate(date_time = ymd_hms(paste(date, time))) %>% 
+        mutate(Oshikko = case_when(Oshikko_suc == 1 ~ 1, 
+                                   Oshikko_suc == 0 ~ 0, 
+                                   TRUE ~ NA_real_)) %>% 
+        mutate(Unchi = case_when(Unchi_suc == 1 ~ 1, 
+                                 Unchi_suc == 0 ~ 0, 
+                                 TRUE ~ NA_real_))
+    
+    ## make long data
+    dat2_long <- dat2 %>% 
+        select(date, time, Oshikko, Unchi) %>% 
+        pivot_longer(cols = c(Oshikko, Unchi), 
+                     names_to = "Peeing", 
+                     values_to = "toilet_flag") %>%
+        na.omit() %>% 
+        mutate(time = as_datetime(time))
+    
+    baron_data <- reactive({
+        filter(dat2_long, between(date ,input$date[1], input$date[2]))
     })
-
-    output$distPlot <- renderPlot({
-
-        # draw the histogram with the specified number of bins
-        hist(faithful[, 2], breaks = bins(), col = isolate(input$color), border = 'white')
-
+    
+    output$freqPlot <- renderPlot({
+        
+        # bar plot
+        ggplot(data = baron_data(), 
+               mapping = aes(x = date, colour = Peeing)) + 
+            geom_bar(alpha = 0.5) +
+            scale_x_date(date_breaks = "1 days", date_labels = "%m-%d") + 
+            facet_wrap(~ Peeing) + 
+            scale_y_continuous(breaks = seq(0, 100, by = 1))
+            
     })
-
+    
 })
