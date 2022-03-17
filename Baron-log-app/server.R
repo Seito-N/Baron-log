53f8fe6da0943a8e8efa503de147aa160b7b7c45
Seito Nakamura <74122643+Seito-N@users.noreply.github.com>
2022-03-18 01:43:20 +0900

    add point plot

--- Baron-log-app/server.R
+++ Baron-log-app/server.R
@@ -11,27 +11,28 @@ shinyServer(function(input, output) {
     
     ## make data
     dat2 <- dat %>% 
-        mutate(date = ymd(date)) %>% 
-        mutate(time = hms::as_hms(time)) %>% 
-        mutate(date_time = ymd_hms(paste(date, time))) %>% 
-        mutate(Oshikko = case_when(Oshikko_suc == 1 ~ 1, 
+        mutate(date = ymd(date),
+               time = hms::as_hms(time),
+               date_time = ymd_hms(paste(date, time)), 
+               Oshikko = case_when(Oshikko_suc == 1 ~ 1, 
                                    Oshikko_suc == 0 ~ 0, 
-                                   TRUE ~ NA_real_)) %>% 
-        mutate(Unchi = case_when(Unchi_suc == 1 ~ 1, 
+                                   TRUE ~ NA_real_),
+               Unchi = case_when(Unchi_suc == 1 ~ 1, 
                                  Unchi_suc == 0 ~ 0, 
-                                 TRUE ~ NA_real_)) %>% 
-        mutate(out = case_when(out == 1 ~ "out",
-                               TRUE ~ "in"))
+                                 TRUE ~ NA_real_),
+               out = case_when(out == 1 ~ "out",
+                               TRUE ~ "in")
+               )
     
     ## make long data
     dat2_long <- dat2 %>% 
         select(date, time, Oshikko, Unchi, out) %>% 
         pivot_longer(cols = c(Oshikko, Unchi), 
                      names_to = "Peeing", 
-                     values_to = "toilet_flag") %>%
+                     values_to = "toilet_flag") %>% 
         na.omit() %>% 
         mutate(time = as_datetime(time))
-    
+
     baron_data <- reactive({
         filter(dat2_long, between(date ,input$date[1], input$date[2]))
     })
@@ -47,6 +48,20 @@ shinyServer(function(input, output) {
             theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
             facet_wrap(~ Peeing)
             
+    })
+    
+    output$DoPlot <- renderPlot({
+        
+        ggplot(data = baron_data(), 
+               mapping = aes(x = date, y = time, 
+                             group = interaction(Peeing, out),
+                             colour = Peeing, shape = out)) + 
+            geom_point(size = 6) + 
+            scale_x_date(date_breaks = "1 days", date_labels = "%m-%d") + 
+            scale_y_datetime(breaks = scales::date_breaks("1 hour"),
+                             date_labels = "%H:%M") + 
+            theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+            facet_wrap(~ Peeing)
     })
     
     summary_table <- reactive({
--- Baron-log-app/ui.R
+++ Baron-log-app/ui.R
@@ -14,20 +14,27 @@ shinyUI(fluidPage(
                ## select the date
                dateRangeInput(inputId = "date", 
                               label = "集計期間", 
-                              start = "2022-02-01", 
+                              start = "2022-03-01", 
                               end = "2022-12-31",
                               min = "2021-11-15",
                               max = "2100-1-1",
                               weekstart = 0)
                ),
+        
         column(12, 
                ## show bar plot for freqency
-               mainPanel(plotOutput(outputId = "freqPlot")))
-        ), 
-    fluidRow(
+               mainPanel(plotOutput(outputId = "freqPlot"))
+               ),
+        
+        column(12,
+               ## show plot for doing
+               mainPanel(plotOutput(outputId = "DoPlot"))
+               ),
+        
         column(12, 
                # show the data table
-               mainPanel(DT::dataTableOutput("summary_table")))
+               mainPanel(DT::dataTableOutput("summary_table"))
+               )
         )
     )
     )
